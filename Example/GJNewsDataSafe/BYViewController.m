35ecd65b9bbcdc5efe20338074b147c270fd6534
吴伟 <wuwei@wuweideMacBook-Air.local>
2020-02-11 16:25:06 +0800

    修改test111库

--- Example/GJNewsDataSafe/BYViewController.m
+++ Example/GJNewsDataSafe/BYViewController.m
@@ -21,6 +21,86 @@
 	
 }
 
+
+- (void)test1 {
+    NSArray *json =@[@{@"id": @1,
+                       @"imageId": @"13",
+                       @"time":  @"2016.12.11",
+                       @"status": @"1",
+                       @"data": @[@{@"name": @"张三",
+                                    @"sex": @"男",
+                                    @"number": @1
+                                   },
+                                  @{@"name": @"张三",
+                                   @"sex": @"男",
+                                   @"number": @1
+                                  },
+                                  @{@"name": @"张三",
+                                   @"sex": @"男",
+                                   @"number": @1
+                                  }
+                                ]
+                      }
+                    ];
+  
+    NSArray *validator = @[@{@"id": [NSNumber class],
+                             @"imageId": [NSString class],
+                             @"time": [NSString class],
+                             @"status": [NSString class],
+                             @"data": @[@{@"name": [NSString class],
+                                        @"sex": [NSString class],
+                                        @"number": [NSNumber class]}
+                                       ]
+                             }
+                          ];
+    BOOL result = [GJNewsDataSafe validateJSON:json withValidator:validator];
+    
+    if (result) {
+        NSLog(@"test1数据校验成功！！！！！");
+    } else {
+        NSLog(@"test1数据校验失败！！！！！");
+    }
+}
+
+- (void)test2 {
+    NSArray *json =@[@{@"id": @1,
+                       @"imageId": @"13",
+                       @"time":  @"2016.12.11",
+                       @"status": @"1",
+                       @"data": @[@{@"name": @"张三",
+                                    @"sex": @"男",
+                                    @"number": @1
+                                   },
+                                  @{@"name": @"张三",
+                                   @"sex": @"男",
+                                   @"number": @1
+                                  },
+                                  @{@"name": @"张三",
+                                   @"sex": @"男",
+                                   @"number": @1
+                                  }
+                                ]
+                      }
+                    ];
+  
+    NSArray *validator = @[@{@"id": [NSNumber class],
+                             @"status": [NSString class],
+                             @"data": @[@{@"name": [NSString class],
+                                        @"sex": [NSString class],
+                                        @"number": [NSNumber class]}
+                                       ]
+                             }
+                          ];
+    BOOL result = [GJNewsDataSafe validateJSON:json withValidator:validator];
+    
+    if (result) {
+        NSLog(@"test2数据校验成功！！！！！");
+    } else {
+        NSLog(@"test2数据校验失败！！！！！");
+    }
+    
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
